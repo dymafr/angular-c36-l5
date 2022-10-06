14a16b81db8c9c93527a6907f455d00fa08c9039
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-06 12:45:42 +0200

    3 files modified

--- src/app/input-address/input-address.component.ts
+++ src/app/input-address/input-address.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 @Component({
   selector: 'app-input-address',
@@ -16,6 +15,14 @@ export class InputAddressComponent implements AfterViewInit {
   constructor() {}
 
   ngAfterViewInit() {
+    this.map = new mapboxgl.Map({
+      accessToken: this.accessToken,
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 9,
+      center: [-74.5, 40],
+    });
+
     this.autocomplete = new MapboxGeocoder({
       accessToken: this.accessToken,
       mapboxgl: mapboxgl,
@@ -25,14 +32,6 @@ export class InputAddressComponent implements AfterViewInit {
       if (e.result) {
         console.log(e);
       }
-    });
-
-    this.map = new mapboxgl.Map({
-      accessToken: this.accessToken,
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 9,
-      center: [-74.5, 40],
     });
 
     this.map.addControl(this.autocomplete);
